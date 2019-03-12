343dd58f2e88f57d3b83544fc4eba597415b316c
Akinyele Cafe-Febrissy <akinyele.kafe.febrissy@gmail.com>
2019-03-13 02:21:10 +0900

    moving lab tables to a different schema

---
+++ move_labs.sql
@@ -0,0 +1,8 @@
+create schema if not exists lab;
+alter table artist set schema lab;
+alter table artwork set schema lab;
+alter table boats set schema lab;
+alter table customer set schema lab;
+alter table likeartist set schema lab;
+alter table reserves set schema lab;
+alter table sailors set schema lab;
