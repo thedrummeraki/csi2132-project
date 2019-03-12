e91851485bd6194ffafcbfc680bdffadef27e8dd
Akinyele Cafe-Febrissy <akinyele.kafe.febrissy@gmail.com>
2019-03-13 02:29:07 +0900

    Structure SQL updated to follow our database structure

--- db/structure.sql
+++ db/structure.sql
@@ -1,463 +1,140 @@
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
+DROP DATABASE IF EXISTS CSI2132project;
+CREATE DATABASE CSI2132project;
 
-SET default_tablespace = '';
+-- Clean the database
+drop table if exists addresses cascade;
+drop table if exists hotel_chains cascade;
+drop table if exists customers cascade;
+drop table if exists employees cascade;
+drop table if exists bookings cascade;
+drop table if exists hotels cascade;
+drop table if exists rooms cascade;
+drop table if exists amenities cascade;
 
-SET default_with_oids = false;
+-- Create all tables, with the least dependant table first
+CREATE TABLE addresses(
+    street_number int NOT NULL,
+    street_name VARCHAR(100) NOT NULL,
+    postal_code VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
 
---
--- Name: addresses; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.addresses (
-    street_number integer NOT NULL,
-    street_name character varying(100) NOT NULL,
-    postal_code character varying(20) NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    CONSTRAINT pk_address PRIMARY KEY (street_name, street_number, postal_code)
 );
 
+CREATE TABLE hotel_chains(
+    "id" bigserial PRIMARY KEY,
+    contact_email VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    street_name VARCHAR(100) NOT NULL,
+    street_number int NOT NULL,
+    postal_code VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
 
---
--- Name: amenities; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.amenities (
-    id bigint NOT NULL,
-    type character varying(20) NOT NULL,
-    room_number integer,
-    hotel_id integer NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    FOREIGN KEY(street_number, street_name, postal_code) REFERENCES addresses(street_number, street_name, postal_code)
 );
 
+CREATE TABLE customers(
+    sin VARCHAR(20) primary key,
+    full_name VARCHAR(100) NOT NULL,
+    registration_date TIMESTAMP with time zone NOT NULL,
+    street_name VARCHAR(100) NOT NULL,
+    street_number int NOT NULL,
+    postal_code VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
 
---
--- Name: amenities_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
+    email VARCHAR(255) DEFAULT '' NOT NULL,
+    encrypted_password VARCHAR DEFAULT '' NOT NULL,
+    reset_password_token VARCHAR,
+    reset_password_sent_at TIMESTAMP,
+    remember_created_at TIMESTAMP,
 
-CREATE SEQUENCE public.amenities_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: amenities_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.amenities_id_seq OWNED BY public.amenities.id;
-
-
---
--- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.ar_internal_metadata (
-    key character varying NOT NULL,
-    value character varying,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    FOREIGN KEY(street_number, street_name, postal_code) REFERENCES addresses(street_number, street_name, postal_code)
 );
 
+CREATE TABLE bookings(
+    "id" bigserial PRIMARY KEY,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    customer_sin VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
 
---
--- Name: bookings; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.bookings (
-    id bigint NOT NULL,
-    start_date timestamp without time zone NOT NULL,
-    end_date timestamp without time zone NOT NULL,
-    customer_sin character varying(20) NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    FOREIGN KEY(customer_sin) REFERENCES customers(sin)
 );
 
+CREATE TABLE employees(
+    sin VARCHAR(20) PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    role VARCHAR(20) NOT NULL,
+    manager_sin VARCHAR(20) NOT NULL,
+    hotel_id int NOT NULL,
+    street_name VARCHAR(100) NOT NULL,
+    street_number int NOT NULL,
+    postal_code VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
 
---
--- Name: bookings_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
+    email VARCHAR(255) DEFAULT '' NOT NULL,
+    encrypted_password VARCHAR DEFAULT '' NOT NULL,
+    reset_password_token VARCHAR,
+    reset_password_sent_at TIMESTAMP,
+    remember_created_at TIMESTAMP,
 
-CREATE SEQUENCE public.bookings_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: bookings_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.bookings_id_seq OWNED BY public.bookings.id;
-
-
---
--- Name: customers; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.customers (
-    sin character varying(20) NOT NULL,
-    full_name character varying(100) NOT NULL,
-    registration_date timestamp with time zone NOT NULL,
-    street_name character varying(100) NOT NULL,
-    street_number integer NOT NULL,
-    postal_code character varying(20) NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL,
-    email character varying(255) DEFAULT ''::character varying NOT NULL,
-    encrypted_password character varying DEFAULT ''::character varying NOT NULL,
-    reset_password_token character varying,
-    reset_password_sent_at timestamp without time zone,
-    remember_created_at timestamp without time zone
+    FOREIGN KEY(manager_sin) REFERENCES employees(sin),
+    FOREIGN KEY(street_number, street_name, postal_code) REFERENCES addresses(street_number, street_name, postal_code)
 );
 
+CREATE TABLE hotels(
+    "id" bigserial PRIMARY KEY,
+    category INT NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    hotel_chain_id INT NOT NULL,
+    manager_sin VARCHAR(20),
+    street_name VARCHAR(100) NOT NULL,
+    street_number int NOT NULL,
+    postal_code VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
 
---
--- Name: employees; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.employees (
-    sin character varying(20) NOT NULL,
-    full_name character varying(100) NOT NULL,
-    role character varying(20) NOT NULL,
-    manager_sin character varying(20) NOT NULL,
-    hotel_id integer NOT NULL,
-    street_name character varying(100) NOT NULL,
-    street_number integer NOT NULL,
-    postal_code character varying(20) NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL,
-    email character varying(255) DEFAULT ''::character varying NOT NULL,
-    encrypted_password character varying DEFAULT ''::character varying NOT NULL,
-    reset_password_token character varying,
-    reset_password_sent_at timestamp without time zone,
-    remember_created_at timestamp without time zone
+    FOREIGN KEY(hotel_chain_id) REFERENCES hotel_chains(id),
+    FOREIGN KEY(manager_sin) REFERENCES employees(sin),
+    FOREIGN KEY(street_number, street_name, postal_code) REFERENCES addresses(street_number, street_name, postal_code)
 );
 
+CREATE TABLE rooms(
+    room_number INT NOT NULL,
+    price FLOAT NOT NULL,
+    capacity INT NOT NULL,
+    mountain_view BOOLEAN default 'f' NOT NULL,
+    sea_view BOOLEAN default 'f' NOT NULL,
+    is_expandable BOOLEAN default 'f' NOT NULL,
+    booking_id INT,
+    hotel_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
 
---
--- Name: hotel_chains; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.hotel_chains (
-    id bigint NOT NULL,
-    contact_email character varying(255) NOT NULL,
-    phone_number character varying(20) NOT NULL,
-    street_name character varying(100) NOT NULL,
-    street_number integer NOT NULL,
-    postal_code character varying(20) NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    FOREIGN KEY(booking_id) REFERENCES bookings(id),
+    FOREIGN KEY(hotel_id) REFERENCES hotels(id),
+    CONSTRAINT pk_room PRIMARY KEY (room_number, hotel_id)
 );
 
+CREATE TABLE amenities(
+    "id" bigserial PRIMARY KEY,
+    type VARCHAR(20) NOT NULL,
+    room_number INT,
+    hotel_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
 
---
--- Name: hotel_chains_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.hotel_chains_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: hotel_chains_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.hotel_chains_id_seq OWNED BY public.hotel_chains.id;
-
-
---
--- Name: hotels; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.hotels (
-    id bigint NOT NULL,
-    category integer NOT NULL,
-    phone_number character varying(20) NOT NULL,
-    email character varying(255) NOT NULL,
-    hotel_chain_id integer NOT NULL,
-    manager_sin character varying(20),
-    street_name character varying(100) NOT NULL,
-    street_number integer NOT NULL,
-    postal_code character varying(20) NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    FOREIGN KEY(room_number, hotel_id) REFERENCES rooms(room_number, hotel_id)
 );
 
-
---
--- Name: hotels_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.hotels_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: hotels_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.hotels_id_seq OWNED BY public.hotels.id;
-
-
---
--- Name: rooms; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.rooms (
-    room_number integer NOT NULL,
-    price double precision NOT NULL,
-    capacity integer NOT NULL,
-    mountain_view boolean DEFAULT false NOT NULL,
-    sea_view boolean DEFAULT false NOT NULL,
-    is_expandable boolean DEFAULT false NOT NULL,
-    booking_id integer,
-    hotel_id integer NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
-);
-
-
---
--- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.schema_migrations (
-    version character varying NOT NULL
-);
-
-
---
--- Name: amenities id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.amenities ALTER COLUMN id SET DEFAULT nextval('public.amenities_id_seq'::regclass);
-
-
---
--- Name: bookings id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.bookings ALTER COLUMN id SET DEFAULT nextval('public.bookings_id_seq'::regclass);
-
-
---
--- Name: hotel_chains id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.hotel_chains ALTER COLUMN id SET DEFAULT nextval('public.hotel_chains_id_seq'::regclass);
-
-
---
--- Name: hotels id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.hotels ALTER COLUMN id SET DEFAULT nextval('public.hotels_id_seq'::regclass);
-
-
---
--- Name: amenities amenities_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.amenities
-    ADD CONSTRAINT amenities_pkey PRIMARY KEY (id);
-
-
---
--- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.ar_internal_metadata
-    ADD CONSTRAINT ar_internal_metadata_pkey PRIMARY KEY (key);
-
-
---
--- Name: bookings bookings_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.bookings
-    ADD CONSTRAINT bookings_pkey PRIMARY KEY (id);
-
-
---
--- Name: customers customers_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.customers
-    ADD CONSTRAINT customers_pkey PRIMARY KEY (sin);
-
-
---
--- Name: employees employees_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.employees
-    ADD CONSTRAINT employees_pkey PRIMARY KEY (sin);
-
-
---
--- Name: hotel_chains hotel_chains_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.hotel_chains
-    ADD CONSTRAINT hotel_chains_pkey PRIMARY KEY (id);
-
-
---
--- Name: hotels hotels_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.hotels
-    ADD CONSTRAINT hotels_pkey PRIMARY KEY (id);
-
-
---
--- Name: addresses pk_address; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.addresses
-    ADD CONSTRAINT pk_address PRIMARY KEY (street_name, street_number, postal_code);
-
-
---
--- Name: rooms pk_room; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.rooms
-    ADD CONSTRAINT pk_room PRIMARY KEY (room_number, hotel_id);
-
-
---
--- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.schema_migrations
-    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
-
-
---
--- Name: amenities amenities_room_number_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.amenities
-    ADD CONSTRAINT amenities_room_number_fkey FOREIGN KEY (room_number, hotel_id) REFERENCES public.rooms(room_number, hotel_id);
-
-
---
--- Name: bookings bookings_customer_sin_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.bookings
-    ADD CONSTRAINT bookings_customer_sin_fkey FOREIGN KEY (customer_sin) REFERENCES public.customers(sin);
-
-
---
--- Name: customers customers_street_number_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.customers
-    ADD CONSTRAINT customers_street_number_fkey FOREIGN KEY (street_number, street_name, postal_code) REFERENCES public.addresses(street_number, street_name, postal_code);
-
-
---
--- Name: employees employees_manager_sin_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.employees
-    ADD CONSTRAINT employees_manager_sin_fkey FOREIGN KEY (manager_sin) REFERENCES public.employees(sin);
-
-
---
--- Name: employees employees_street_number_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.employees
-    ADD CONSTRAINT employees_street_number_fkey FOREIGN KEY (street_number, street_name, postal_code) REFERENCES public.addresses(street_number, street_name, postal_code);
-
-
---
--- Name: employees fk_employees_hotel_id; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.employees
-    ADD CONSTRAINT fk_employees_hotel_id FOREIGN KEY (hotel_id) REFERENCES public.hotels(id);
-
-
---
--- Name: hotel_chains hotel_chains_street_number_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.hotel_chains
-    ADD CONSTRAINT hotel_chains_street_number_fkey FOREIGN KEY (street_number, street_name, postal_code) REFERENCES public.addresses(street_number, street_name, postal_code);
-
-
---
--- Name: hotels hotels_hotel_chain_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.hotels
-    ADD CONSTRAINT hotels_hotel_chain_id_fkey FOREIGN KEY (hotel_chain_id) REFERENCES public.hotel_chains(id);
-
-
---
--- Name: hotels hotels_manager_sin_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.hotels
-    ADD CONSTRAINT hotels_manager_sin_fkey FOREIGN KEY (manager_sin) REFERENCES public.employees(sin);
-
-
---
--- Name: hotels hotels_street_number_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.hotels
-    ADD CONSTRAINT hotels_street_number_fkey FOREIGN KEY (street_number, street_name, postal_code) REFERENCES public.addresses(street_number, street_name, postal_code);
-
-
---
--- Name: rooms rooms_booking_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.rooms
-    ADD CONSTRAINT rooms_booking_id_fkey FOREIGN KEY (booking_id) REFERENCES public.bookings(id);
-
-
---
--- Name: rooms rooms_hotel_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.rooms
-    ADD CONSTRAINT rooms_hotel_id_fkey FOREIGN KEY (hotel_id) REFERENCES public.hotels(id);
-
-
---
--- PostgreSQL database dump complete
---
-
-SET search_path TO "$user", public;
-
-INSERT INTO "schema_migrations" (version) VALUES
-('20190311180035');
-
-
+-- Alter the tables accordingly
+ALTER TABLE employees add
+  CONSTRAINT "fk_employees_hotel_id"
+  FOREIGN KEY("hotel_id")
+  REFERENCES "hotels"("id");
