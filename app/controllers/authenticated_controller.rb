4ad037ec2330412f447d20575c7014d6057f3cab
Akinyele Cafe-Febrissy <akinyele.kafe.febrissy@gmail.com>
2019-03-31 14:55:24 -0400

    Authenticated controller will need to have either employee or customer on

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,5 +1,13 @@
 class AuthenticatedController < ApplicationController
   layout 'authenticated'
 
-  before_action :authenticate_customer!
+  before_action :authenticate_user!
+
+  private
+
+  def authenticate_user!
+    unless logged_in?
+      redirect_to '/', alert: 'You must be signed in to see this.'
+    end
+  end
 end
