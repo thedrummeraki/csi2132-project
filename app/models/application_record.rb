b2067cdb4767b2d3e67fbf7f24e8be82b11b5349
Akinyele Cafe-Febrissy <akinyele.kafe.febrissy@gmail.com>
2019-03-31 14:59:45 -0400

    Address must properly be updated or created

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,21 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  protected
+
+  def ensure_address!
+    return unless respond_to?(:address)
+
+    puts "Making sure the address already exists"
+    return unless address.nil?
+
+    Address.where(
+      street_number: street_number,
+      street_name: street_name,
+      postal_code: postal_code,
+      city: city,
+      province_state: province_state,
+      country: country
+    ).create
+  end
 end
