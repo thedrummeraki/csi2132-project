166d42acaaf7e70ec32e350a32dda6efaf063d14
Akinyele Cafe-Febrissy <akinyele.kafe.febrissy@gmail.com>
2019-03-31 14:58:59 -0400

    Search result now split in groups of 3

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,18 @@
 class SearchController < ApplicationController
 
+  autocomplete :address, :area
+
   def index
-    @search = Search.new
-    @search.analyze!(search_params)
     @area = search_params[:area]
     @capacity = search_params[:capacity]
-    @hotel_chain_id = search_params[:hotel_chain_id]
+    @hotel_chain_id = search_params[:hotel_chain]
     @category = search_params[:category]
     @size = search_params[:size]
     @price = search_params[:price]
+
+    search = Search.new
+    search.analyze!(search_params)
+    @rooms_parts = search.rooms.each_slice(3).to_a
   end
 
   private
@@ -17,7 +21,7 @@ class SearchController < ApplicationController
     params.require(:search).permit(
       :area,
       :capacity,
-      :hotel_chain_id,
+      :hotel_chain,
       :category,
       :size,
       :price
