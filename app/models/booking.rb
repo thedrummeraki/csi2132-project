973fee2ab456ea8f0cd4428615ccc23695ca9e1e
Akinyele Cafe-Febrissy <akinyele.kafe.febrissy@gmail.com>
2019-04-01 16:31:58 -0400

    booking check in mechanism

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,43 @@
 class Booking < ApplicationRecord
   belongs_to :customer, foreign_key: :customer_sin
   belongs_to :room, foreign_key: [:room_number, :hotel_id]
+
+  # We will be converting a booking into a renting
+  def check_in!
+    ensure_can_check_in!
+    self.update(status: :complete)
+    Renting.create(
+      status: :renting,
+      employee_sin: employee_sin,
+      start_date: start_date,
+      end_date: end_date,
+      customer_sin: customer_sin,
+      room_number: room_number,
+      hotel_id: hotel_id,
+      has_booked: true
+    )
+  end
+
+  def can_check_in?
+    status.to_s == 'started'
+  end
+
+  # Check if a person has made a booking but not checked in
+  # yet (right now)
+  def expired?(range: :now)
+    return false unless persisted?
+    case range
+    when :all
+      !!(start_date <= Time.now && end_date <= Time.now)
+    else
+      !!(start_date <= Time.now)
+    end
+  end
+
+  private
+
+  def ensure_can_check_in!
+    return if can_check_in?
+    raise "This booking is not complete, it is '#{status}', so it can't be checked in."
+  end
 end
